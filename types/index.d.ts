338bcd02a5ae099b37d0a73761f44215b382f680
lzw-leaf <1604748707@qq.com>
2021-06-01 14:28:10 +0800

    增加ts支持

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,9 +3,21 @@ interface AppInfo {
   versionCode: string
 }
 
-interface BuglySdk {
-  checkUpdate(isManual = true, isSilence = false): Promise<string>
-  getAppInfo(): Prmoise<AppInfo>
+interface Bugly {
+  /**
+   * 触发bugly更新检测
+   * @param {boolean} isManual 是否用户手动触发 默认 true
+   * @param {boolean} isSilence 是否静默 （false则弹窗 ,true则不触发）默认 false
+   * @return {Promise<string>} 返回提示信息
+   */
+  checkUpgrade(isManual?: boolean, isSilence?: boolean): Promise<string>
+
+  /**
+   * 获取应用信息
+   * 目前只返回 versionName,versionCode
+   * @return {Promise<{versionName,versionCode}>} 返回应用信息
+   */
+  getAppInfo(): Promise<AppInfo>
 }
 
-declare var BuglySdk: BuglySdk
+declare var BuglySdk: Bugly
